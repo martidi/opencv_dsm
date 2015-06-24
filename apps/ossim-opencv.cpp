f51f776a23b840cdbb988eb7fcf31f360da1e31b
martidi <martina@Martina-Ubuntu.(none)>
2015-06-24 17:57:00 +0200

    space

--- apps/ossim-opencv.cpp
+++ apps/ossim-opencv.cpp
@@ -268,86 +268,118 @@ int main(int argc,  char* argv[])
    			//test_nadfwd->execute();
    						
 /*  			
-			// Conversion factor (from pixels to meters) computation
+			// CONVERSION FACTOR (from pixels to meters) COMPUTATION *************
 			ossimRefPtr<ossimImageGeometry> raw_master_geom = raw_master_handler->getImageGeometry();    
 			ossimRefPtr<ossimImageGeometry> raw_slave_geom = raw_slave_handler->getImageGeometry(); 
-			
-			ossimGpt ul,ur,lr,ll;
-			raw_master_geom->getCornerGpts(ul, ur, lr, ll);
-					
-			double Dlon = (ur.lon - ul.lon)/2.0;
-			double Dlat = (ul.lat - ll.lat)/2.0;
-        
-			cv::Mat conv_factor = cv::Mat::zeros(3,3, CV_64F);
-			
-			for (int i=0 ; i<3 ; i++) //LAT
-			{
-				for (int j=0 ; j<3 ; j++) //LON
-				{
-					ossimGpt punto_terra(ur.lat-i*Dlat,ul.lon+j*Dlon,200.00);
-					ossimGpt punto_terra_up(ur.lat-i*Dlat,ul.lon+j*Dlon,2200.00);	
-					ossimDpt punto_img(0.,0.);
-					ossimDpt punto_img_up(0.,0.);
-				
-					raw_master_geom->worldToLocal(punto_terra,punto_img);              
-					raw_master_geom->worldToLocal(punto_terra_up,punto_img_up);   
-					
-					double DeltaI_Master = punto_img_up.x - punto_img.x;
-					double DeltaJ_Master = punto_img_up.y - punto_img.y;
-        
-					raw_slave_geom->worldToLocal(punto_terra,punto_img);       
-					raw_slave_geom->worldToLocal(punto_terra_up,punto_img_up);    
-					
-					double DeltaI_Slave = punto_img_up.x - punto_img.x;
-					double DeltaJ_Slave = punto_img_up.y - punto_img.y;
-					
-					conv_factor.at<double>(i,j) = DeltaJ_Slave - DeltaJ_Master;
-				}			
-			}
-	
-			cv::Scalar mean_conv_factor, stDev_conv_factor;
-			cv::meanStdDev(conv_factor, mean_conv_factor, stDev_conv_factor);
-
-			double stDev_conversionF = stDev_conv_factor.val[0];
-			double mean_conversionF = mean_conv_factor.val[0]/(2200.00-200.00);	        
-			
-			cout << "Conversion Factor from pixels to meters\t" << mean_conversionF <<endl;
-			cout << "Standard deviation Conversion Factor\t" << stDev_conversionF <<endl;
-			
-			
-			char * prova = ap[4];			
-			string log(prova);	
+          
+            // Conversion factor computed on tile and not over all the image
+			double Dlon = (lon_max - lon_min)/2.0;
+			double Dlat = (lat_max - lat_min)/2.0;
+						
+			// Getting ready the log file
+			char * logfile = ap[3];			
+			string log(logfile);	
 			log.erase(log.end()-4, log.end()-0 );
 			log = log + "_logfile.txt";			
 			
 			// Creating and writing the log file
 			ofstream myfile;
 			myfile.open (log.c_str());
-			myfile << "Master orthorectification parameters" <<endl;
+				
+			cv::Mat conv_factor_J = cv::Mat::zeros(3,3, CV_64F);				
+			cv::Mat conv_factor_I = cv::Mat::zeros(3,3, CV_64F);	
+
+            ossimGpt central_object_point(46.0490, 11.1053);
+            ossimDpt central_image_point(0.,0.);
+            ossimGpt point_object(0., 0.);
+
+            raw_master_geom->worldToLocal(central_object_point, central_image_point);
+            cout << central_image_point << endl; //coordinate immagine corrispondenti al punto centrale
+
+            raw_master_geom->localToWorld(central_image_point, 1400, point_object);
+            cout << point_object << endl;  // coordinate oggetto corrispondenti al punto centrale proiettato a 1400 m
+
+			for (int i=0 ; i<3 ; i++) //LAT
+			{
+				for (int j=0 ; j<3 ; j++) //LON
+				{
+                    ossimGpt punto_terra(lat_max-i*Dlat,lon_min+j*Dlon,MinHeight -50);
+                    ossimGpt punto_terra_up(lat_max-i*Dlat,lon_min+j*Dlon,MaxHeight + 50);
+					
+					ossimDpt punto_img(0.,0.);
+					ossimDpt punto_img_up(0.,0.);
+				
+					raw_master_geom->worldToLocal(punto_terra,punto_img);        //con qst trasf ottengo punto_img della master      
+                    raw_master_geom->worldToLocal(punto_terra_up,punto_img_up);
+
+					myfile << "MASTER IMAGE" << "\t" << "Ground point" << punto_terra << "\t" << "Image point" << punto_img << "\t" << "Ground point up" << punto_terra_up << "\t" << "Image point up" << punto_img_up << "\t";    
+					
+					double DeltaI_Master = punto_img_up.x - punto_img.x;
+					double DeltaJ_Master = punto_img_up.y - punto_img.y;
+					    
+					raw_slave_geom->worldToLocal(punto_terra,punto_img);       
+					raw_slave_geom->worldToLocal(punto_terra_up,punto_img_up);    
+
+					myfile << "SLAVE IMAGE" << "\t" << "Ground point" << punto_terra << "\t" << "Image point" << punto_img << "\t" << "Ground point up" << punto_terra_up << "\t" << "Image point up" << punto_img_up << "\t" << endl;    
+					
+					double DeltaI_Slave = punto_img_up.x - punto_img.x;
+					double DeltaJ_Slave = punto_img_up.y - punto_img.y;
+					
+					conv_factor_J.at<double>(i,j) = DeltaJ_Slave - DeltaJ_Master; // conv_factor for ACROSS-track imgs
+					conv_factor_I.at<double>(i,j) = DeltaI_Slave - DeltaI_Master; // conv_factor for ALONG-track imgs 
+				}			
+			}
+			
+			//cout << conv_factor_J << endl;
+			//cout << conv_factor_I << endl;	
+									
+			cv::Scalar mean_conv_factor_J, stDev_conv_factor_J;
+			cv::meanStdDev(conv_factor_J, mean_conv_factor_J, stDev_conv_factor_J);
+			
+			cv::Scalar mean_conv_factor_I, stDev_conv_factor_I;
+			cv::meanStdDev(conv_factor_I, mean_conv_factor_I, stDev_conv_factor_I);			
+
+			double stDev_conversionF_J = stDev_conv_factor_J.val[0];
+            double mean_conversionF_J = mean_conv_factor_J.val[0]/(MaxHeight -MinHeight + 100);
+	
+			double stDev_conversionF_I = stDev_conv_factor_I.val[0];
+            double mean_conversionF_I = mean_conv_factor_I.val[0]/(MaxHeight -MinHeight + 100);
+			
+            double mean_conversionF = (sqrt((mean_conv_factor_J.val[0]*mean_conv_factor_J.val[0]) + (mean_conv_factor_I.val[0]*mean_conv_factor_I.val[0]))/(MaxHeight -MinHeight + 100));
+			
+			cout << "J Conversion Factor from pixels to meters\t" << mean_conversionF_J << endl;
+			cout << "Standard deviation J Conversion Factor\t" << stDev_conversionF_J << endl << endl;
+
+			cout << "I Conversion Factor from pixels to meters\t" << mean_conversionF_I << endl;
+			cout << "Standard deviation I Conversion Factor\t" << stDev_conversionF_I << endl << endl;
+			
+			cout << "Total Conversion Factor from pixels to meters\t" << mean_conversionF << endl;
+			// END CONVERSION FACTOR COMPUTATION ****************************************
+			
+			myfile << endl << "Master orthorectification parameters" <<endl;
 			myfile << master_key << endl;
 			myfile << "Slave orthorectification parameters" <<endl;
 			myfile << slave_key << endl;
-			myfile <<"Conversion Factor from pixels to meters\t" << mean_conversionF <<endl;
-			myfile <<"Standard deviation Conversion Factor\t" << stDev_conversionF <<endl; 
-			myfile.close();		
-			* /	
+			myfile <<"Conversion Factor from pixels to meters\t" << mean_conversionF_J <<endl;
+			myfile <<"Standard deviation Conversion Factor\t" << stDev_conversionF_J <<endl; 
+			myfile.close();			
 						
 /*			
-		cv::Mat parallax = cv::Mat::zeros(good_matches.size(), 1, CV_64F);
-		for(size_t i = 0; i < good_matches.size(); i++)
-		{
-			parallax.at<double>(i,0) = keypoints1[good_matches[i].queryIdx].pt.y - keypoints2[good_matches[i].trainIdx].pt.y; 	
-		}		
-		cv::Scalar mean_parallax, stDev_parallax;
-		cv::meanStdDev(parallax, mean_parallax, stDev_parallax);
+            cv::Mat parallax = cv::Mat::zeros(good_matches.size(), 1, CV_64F);
+            for(size_t i = 0; i < good_matches.size(); i++)
+            {
+                parallax.at<double>(i,0) = keypoints1[good_matches[i].queryIdx].pt.y - keypoints2[good_matches[i].trainIdx].pt.y;
+            }
+            cv::Scalar mean_parallax, stDev_parallax;
+            cv::meanStdDev(parallax, mean_parallax, stDev_parallax);
 		
-		double dev_y = stDev_parallax.val[0]; 	
-		double mean_diff_y = mean_parallax.val[0]; 
+            double dev_y = stDev_parallax.val[0];
+            double mean_diff_y = mean_parallax.val[0];
 	
-		cout << "dev_y = " << dev_y << endl
-	         << "mean_diff_y = " << mean_diff_y << endl;			
+            cout << "dev_y = " << dev_y << endl
+                 << "mean_diff_y = " << mean_diff_y << endl;
 */				
-			/*	
+/*				
 			// From Disparity to DSM
 			ossimImageGeometry* master_geom = master_handler->getImageGeometry().get();			
 			test->computeDSM(mean_conversionF, elev, master_geom);
@@ -359,7 +391,7 @@ int main(int argc,  char* argv[])
 			writer->connectMyInputTo(0, handler_disp);
 			writer->execute();
             
-			delete writer;  */
+			delete writer; */
 			delete test_bwdnad;	
 			//delete test_nadfwd;			
 		}
